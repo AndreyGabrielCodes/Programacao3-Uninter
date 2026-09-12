6f47e14e743d029c34fe27843a74870d3c87ed21
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2026-09-12 15:57:27 -0300

    Correções realizadas - Finalizada Questão 01
    Realizadas correções conforme erros encontrados durante a execução do sistema - Realizado melhorias de Layout

--- Questao1.py
+++ Questao1.py
@@ -1,29 +1,32 @@
 #FEITO POR ANDREY GABRIEL DE ANDRADE MORAES
 #4731589
 
-LETRA_PRIORIDADE = 'A'
-LETRA_SEMPRIORIDADE = 'V'
+LETRA_COR_PRIORIDADE = 'A'
+LETRA_COR_SEM_PRIORIDADE = 'V'
 COR_PRIORIDADE = 'Amarelo'
-COR_SEMPRIORIDAE = 'Verde'
+COR_SEM_PRIORIDADE = 'Verde'
 
 SenhaAtualPrioridade = 201
 SenhaAtualSemPrioridade = 1
 
 def retorna_senha_cor(prioridade):
-    if prioridade == LETRA_PRIORIDADE:
+
+    global SenhaAtualPrioridade, SenhaAtualSemPrioridade
+
+    if prioridade == LETRA_COR_PRIORIDADE:
         retorno = SenhaAtualPrioridade
-        SenhaAtualPrioridade += SenhaAtualPrioridade
+        SenhaAtualPrioridade += 1
         return retorno;
-    elif prioridade == LETRA_SEMPRIORIDADE:
+    elif prioridade == LETRA_COR_SEM_PRIORIDADE:
         retorno = SenhaAtualSemPrioridade
-        SenhaAtualSemPrioridade += SenhaAtualSemPrioridade
+        SenhaAtualSemPrioridade += 1
         return retorno;
 
 def retorna_descricao_cor(prioridade):
-    if prioridade == LETRA_PRIORIDADE:
+    if prioridade == LETRA_COR_PRIORIDADE:
         return COR_PRIORIDADE;
-    elif prioridade == LETRA_SEMPRIORIDADE:
-        return COR_SEMPRIORIDAE;
+    elif prioridade == LETRA_COR_SEM_PRIORIDADE:
+        return COR_SEM_PRIORIDADE;
 
 class NoSenhaPaciente:
     def __init__(self,cor):
@@ -49,71 +52,80 @@ class ListaEncadeadaPacientes:
             atual = atual.proximo
         atual.proximo = no_novo
 
-        print('Paciente sem prioridade inserido!')
+        print('\nPaciente sem prioridade inserido!')
 
     def inserirComPrioridade(self,no_novo):
         """Insere o nó de prioridade após todos os nós de prioridade já existentes"""
 
         #Lista vazia ou com head sem prioridade
-        if not self.head or self.head.prioridade == LETRA_SEMPRIORIDADE:
+        if not self.head or self.head.cor == LETRA_COR_SEM_PRIORIDADE:
             no_novo.proximo = self.head
             self.head = no_novo
-            print('Paciente com prioridade inserido!')
+            print('\nPaciente com prioridade inserido!')
             return
 
         atual = self.head
 
-        while atual.proximo and atual.proximo.prioridade == LETRA_PRIORIDADE:
+        while atual.proximo and atual.proximo.cor == LETRA_COR_PRIORIDADE:
             atual = atual.proximo
 
         #Insere o novo conteudo após o ultimo nó de prioridade encontrado
         no_novo.proximo = atual.proximo
         atual.proximo = no_novo
 
-        print('Paciente com prioridade inserido!')
+        print('\nPaciente com prioridade inserido!')
 
     def atenderPaciente(self):
         """Retira o primeiro paciente da fila e imprime chamada para atendimento"""
 
         if not self.head:
-            raise("Não existentes pacientes na fila para chamar no momento!")
+            raise Exception("Não existentes pacientes na fila para chamar no momento!")
 
         paciente_atendido = self.head
 
         #Retira o primeiro paciente e faz a lista apontar para o próximo
         self.head = self.head.proximo
 
-        mensagem = f"Atendendo o paciente cor {paciente_atendido.cor} ({retorna_descricao_cor(paciente_atendido.cor)}) e número {paciente_atendido.numero}"
+        mensagem = f"\nAtendendo o paciente cor {paciente_atendido.cor} ({retorna_descricao_cor(paciente_atendido.cor)}) e número {paciente_atendido.numero}"
         print(mensagem)
 
     def imprimirListaEspera(self):
+
+        if not self.head:
+            raise Exception("Não existentes pacientes na fila no momento!")
+        
         elementos_listagem = []
         atual = self.head
 
         while atual:
-            elementos_listagem(f"[{atual.prioridade},{atual.numero}]")
+            elementos_listagem.append(f"[{atual.cor},{atual.numero}]")
             atual = atual.proximo
 
-        print("Lista -> " + " ".join(elementos_listagem))
+        print("\nLista -> " + " ".join(elementos_listagem))
 
 FilaPacientes = ListaEncadeadaPacientes()
 
 def inserir():
 
-    prioridade = input(f"Informe a cor/prioridade da senha ({LETRA_PRIORIDADE}/{LETRA_SEMPRIORIDADE}): ")
-    prioridade.upper()
+    print('\nMENU - OPÇÕES DE SENHA')
+    print(f'{LETRA_COR_PRIORIDADE} = {COR_PRIORIDADE} - Com prioridade')
+    print(f'{LETRA_COR_SEM_PRIORIDADE} =  {COR_SEM_PRIORIDADE}  - Sem prioridade')
+    print('')
 
-    if prioridade not in (LETRA_PRIORIDADE,LETRA_SEMPRIORIDADE):
-        raise ValueError("Prioridade informada não existe!")
+    prioridade = input(f"Informe a cor/prioridade da senha ({LETRA_COR_PRIORIDADE}/{LETRA_COR_SEM_PRIORIDADE}): ")
+    prioridade = prioridade.upper()
+
+    if prioridade not in (LETRA_COR_PRIORIDADE,LETRA_COR_SEM_PRIORIDADE):
+        raise Exception("Prioridade informada não existe!")
 
     paciente_novo = NoSenhaPaciente(prioridade)
 
-    if prioridade == LETRA_SEMPRIORIDADE:
+    if prioridade == LETRA_COR_SEM_PRIORIDADE:
         FilaPacientes.inserirSemPrioridade(paciente_novo)
     else:
         FilaPacientes.inserirComPrioridade(paciente_novo)
 
-    print(f'Inserido paciente de senha {paciente_novo.numero} e cor {paciente_novo.cor} ({retorna_descricao_cor(paciente_novo.cor)})')
+    print(f'\nInserido paciente de senha {paciente_novo.numero} e cor {paciente_novo.cor} ({retorna_descricao_cor(paciente_novo.cor)})')
 
 
 #Main
@@ -125,6 +137,7 @@ while True:
 
     try:
         print('')
+        print('MENU PRINCIPAL')
         print('1 - Adicionar paciente a fila')
         print('2 - Mostrar pacientes na fila')
         print('3 - Chamar paciente')
@@ -133,21 +146,19 @@ while True:
 
         opcao = input('Informe a opção desejada: ')
 
-        print('')
-
         match opcao:
-            case 1:
+            case '1':
                 inserir()
-            case 2:
+            case '2':
                 FilaPacientes.imprimirListaEspera()
-            case 3:
+            case '3':
                 FilaPacientes.atenderPaciente()
-            case 4:
+            case '4':
+                print('\nEncerrando sistema...')
                 break
             case _: 
-                raise ValueError('Opção escolhida não existe, tente novamente')
+                raise Exception('Opção escolhida não existe, tente novamente!')
 
     except Exception as e:
-        print(e)
-    else:
-        print('Encerrando sistema...')   
+        print('')
+        print('Erro! - ' + str(e))
